5e129c2456b4ca4ea65b63295250a836f83b7b15
root <root@puppet.codespaces.io>
2017-08-14 05:53:57 +0000

    creting specs for dev env

--- manifests/app.pp
+++ manifests/app.pp
@@ -1,7 +1,7 @@
 hiera_include('classes')
 
 
-node 'node1' {
+node 'node3' {
 
   # include base
   include tomcat 
@@ -12,27 +12,6 @@ node 'node1' {
   }
 }
 
-node default {
-
-  notify{"checkpoint_1":
-  
-    
-      message => "  *** DEFAULT BLOCK *** ",
-  }
-
-
-}
-
-
-node 'node2' {
-
-  # include base
-  include tomcat 
-  
-  tomcat::deploy { "sysfoo":
-    deploy_url       => 'https://10-94848332-gh.circle-artifacts.com/0/tmp/circle-artifacts.Q1EpcWu/sysfoo.war',
-    checksum_value   => '4baa77eb5efd7c92edce03feffbcf098'
-  }
 }
 
 
